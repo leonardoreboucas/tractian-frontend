985df51595730113d31fd8ab4b3b8a848ba28f21
leonardoreboucas <leonardo_r_carvalho@yahoo.com.br>
2022-02-28 18:12:24 -0300

    Add history chart

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,18 +7,77 @@ import RecentTable from "@/components/RecentTable";
 import { request } from "@/request";
 import { LoadingOutlined } from '@ant-design/icons';
 import PageLoader from "@/components/PageLoader";
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+
 
 
 export default function Dashboard() {
   const [control, setControl] = useState(false)
+  const [controlHistory, setControlHistory] = useState(false)
   const [assets, setAssets] = useState([])
+  const [options, setOptions] = useState({})
   useEffect(() => {
     const resAssets = request.list('asset').then(res => {
       setAssets(res.result)
     })
   },[control]); 
+  useEffect(() => {
+    const resHistory = request.list('asset/history').then(res => {
+      let processed = []
+      if (res.result){
+        processed = Object.keys(res.result).map((row)=>{
+          return {
+            name: `${res.result[row].name} (${res.result[row]._id})`,
+            data: res.result[row].history.map(r=>[new Date(r.date).toString(),r.health_level])
+          }
+        })
+      }
+        setOptions({
+          chart: {
+            type: 'line',
+            width: 1000,
+            align: 'center'
+          },
+        
+          title: {
+            text: 'Health level history'
+          },
+        
+          yAxis: {
+            title: {
+                text: 'Health level'
+            }
+        },
+        
+        xAxis: {
+            title: {
+                text: 'Register'
+            }
+        },
+        
+        series: processed,
+        })
+      })
+     
+  },[controlHistory]); 
+
+
+
+  
+
   return (
     <DashboardLayout>
+      <Row gutter={[24, 24]}>
+        <HighchartsReact
+          highcharts={Highcharts}
+          options={options}
+          width="100%"
+        />
+      </Row>
+
+      <Divider />
+
       <Row gutter={[24, 24]}>
         {(assets && assets.length) ? assets.map((row, idx) => (
           <Col key={idx} className="gutter-row" span={6}>
@@ -28,11 +87,14 @@ export default function Dashboard() {
                 style={{ textAlign: "center", justifyContent: "center" }}
               >
                 <img src={row.image[0].response.result} width="50em" />
-                <h3 style={{ color: "#22075e", marginBottom: 30 }}>
+                <h3 style={{ color: "#22075e" }}>
                   {row.name}
                 </h3>
+                <h6 style={{ color: "#22075e", marginBottom: 30 }}>
+                  {row._id}
+                </h6>
 
-                <Progress type="dashboard" percent={row.health_level} width={148} />
+                <Progress type="dashboard" percent={row.health_level} width={118} />
                   <p>Health level</p>
                 <Divider />
 
